1629fff9785730172732bce3f4e369f0bc25118c
hyorinkim <201802085@cnu.o.ac.kr>
2022-11-01 16:00:19 +0900

    ClubServiceLogic 클래스 생성 및 interface 상속

--- travel-club-spring-core/src/main/java/io/namoosori/travelclub/spring/service/logic/ClubServiceLogic.java
+++ travel-club-spring-core/src/main/java/io/namoosori/travelclub/spring/service/logic/ClubServiceLogic.java
@@ -1,2 +1,35 @@
-package io.namoosori.travelclub.spring.service.logic;public class ClubServiceLogic {
+package io.namoosori.travelclub.spring.service.logic;
+
+import io.namoosori.travelclub.spring.aggregate.club.TravelClub;
+import io.namoosori.travelclub.spring.service.ClubService;
+import io.namoosori.travelclub.spring.service.sdo.TravelClubCdo;
+import io.namoosori.travelclub.spring.shared.NameValueList;
+
+import java.util.List;
+
+public class ClubServiceLogic implements ClubService {
+    @Override
+    public String registerClub(TravelClubCdo club) {
+        return null;
+    }
+
+    @Override
+    public TravelClub findClubById(String id) {
+        return null;
+    }
+
+    @Override
+    public List<TravelClub> findClubsByName(String name) {
+        return null;
+    }
+
+    @Override
+    public void modify(String clubId, NameValueList nameValues) {
+
+    }
+
+    @Override
+    public void remove(String clubId) {
+
+    }
 }
