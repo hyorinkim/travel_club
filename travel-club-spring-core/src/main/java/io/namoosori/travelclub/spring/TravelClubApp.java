2cb733167dce51712979756caf4b33de49a0f3a7
hyorinkim <201802085@cnu.o.ac.kr>
2022-11-01 16:28:14 +0900

    bean 등록에 대한 정보를 spring 에게 정보를 전달하는 코드작성

--- travel-club-spring-core/src/main/java/io/namoosori/travelclub/spring/TravelClubApp.java
+++ travel-club-spring-core/src/main/java/io/namoosori/travelclub/spring/TravelClubApp.java
@@ -1,7 +1,21 @@
 package io.namoosori.travelclub.spring;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class TravelClubApp {
     public static void main(String[] args) {
-        
+        //프로그램 시작되면 entry point가 된다.
+        //applicationContext.xml 정보를 spring한테 알려줘야하니까 넘겨주는 코드가 있어야한다.
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        //ApplicationContext 클래스타입임
+        //spring 프레임워크에게 같은 ContextPath 안에 applicationContext.xml안에 있다라고 알려주는 코드
+        //JavaClass를 이용하는 방법도 있음
+
+        String [] beanNames =context.getBeanDefinitionNames();
+        System.out.println(Arrays.toString(beanNames));
     }
 }
